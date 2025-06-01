0cf3a99aaa041e7391025f38327fd24020d7e719
Manje Anand <manjeanand@gmail.com>
2025-06-01 10:50:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Add background to navbar on scroll
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("scrolled", window.scrollY > 50);
+  });
+  
